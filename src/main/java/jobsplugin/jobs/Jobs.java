be99ffd5d8fd87e441959c0dd1fba360d6111d2c
Kapsreiter <s.kapsreiter@stud.hs-wismar.de>
2022-05-15 18:23:33 +0200

    Initial commit

---
+++ src/main/java/jobsplugin/jobs/Jobs.java
@@ -0,0 +1,17 @@
+package jobsplugin.jobs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Jobs extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
